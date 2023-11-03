23ab140912140cc67d5696258a275a46dd26f701
Eliane Puerta <eliane.puerta95@gmail.com>
2023-11-03 11:05:21 -0400

    add component for todo item

---
+++ src/components/TodoListElement.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- the component element for each todo in the list, this way is easier to handle individual actions on each items -->
+  <li :class="{ completed: item?.completed, editing: onEdit }" @dblclick="startEdition">
+    <div class="view">
+      <input
+        class="toggle"
+        type="checkbox"
+        :checked="item?.completed"
+        @click="toggleTodo(item.id)"
+      />
+      <label>{{ item?.title }}</label>
+      <button class="destroy" @click="clearTodo(item.id)"></button>
+    </div>
+    <!-- input for editing the item -->
+    <input
+      id="edit"
+      ref="edit"
+      class="edit"
+      v-model="inputValue"
+      @keyup.enter="saveEdition"
+      @keyup.esc="finishEdition"
+      @blur="onFocusLost"
+    />
+  </li>
+</template>
+
+<script>
+  import { mapMutations } from 'vuex';
+
+  export default {
+    name: 'TodoListElement',
+    props: ['item'],
+    data() {
+      return {
+        onEdit: false,
+        inputValue: '',
+      };
+    },
+    methods: {
+      ...mapMutations(['editTodo', 'clearTodo', 'toggleTodo']),
+      startEdition() {
+        this.onEdit = true;
+        this.inputValue = this.item.title;
+        this.$nextTick(() => this.$refs.edit.focus());
+      },
+      saveEdition() {
+        this.editTodo({ id: this.item.id, title: this.inputValue });
+        this.finishEdition();
+      },
+      finishEdition() {
+        this.inputValue = '';
+        this.onEdit = false;
+      },
+      onFocusLost() {
+        if (this.onEdit) this.saveEdition();
+      },
+    },
+  };
+</script>
+
+<style scoped></style>
