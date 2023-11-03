06e621822dcd01a2ec4b3b82036493b329c7727d
Eliane Puerta <eliane.puerta95@gmail.com>
2023-11-03 11:04:02 -0400

    set up vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,44 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+const STORAGE_KEY = 'todos_storage';
+
+const save = (todos) => localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
 
 export default createStore({
   state: {
-  },
-  getters: {
+    todos: JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'),
   },
   mutations: {
+    addTodo(state, todo) {
+      state.todos.push(todo);
+      save(state.todos);
+    },
+    editTodo(state, { id, title }) {
+      state.todos.find((item) => item.id === id).title = title;
+      save(state.todos);
+    },
+    toggleTodo(state, id) {
+      const todo = state.todos.find((item) => item.id === id);
+      todo.completed = !todo.completed;
+      save(state.todos);
+    },
+    clearTodo(state, id) {
+      const index = state.todos.findIndex((item) => item.id === id);
+      state.todos.splice(index, 1);
+      save(state.todos);
+    },
+    toggleAll(state) {
+      const allCompleted =
+        state.todos.filter((item) => item.completed).length === state.todos.length;
+      state.todos.forEach((item) => {
+        item.completed = !allCompleted;
+      });
+      save(state.todos);
+    },
+    clearCompleted(state) {
+      const active = state.todos.filter((item) => !item.completed);
+      state.todos = active;
+      save(state.todos);
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+});
+
